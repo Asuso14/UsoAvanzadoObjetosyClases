36366898f5f2168a1077fa91f0e9496a52c4fbc0
NASHELAPTOP\Usuario <nachogamallolafon123@gmail.com>
2025-02-20 16:25:45 +0100

    Clases anónimas dispositivo

--- ObjetosAvanzados/src/main/java/org/example/DispositivosInteligentes/AppDispositivos.java
+++ ObjetosAvanzados/src/main/java/org/example/DispositivosInteligentes/AppDispositivos.java
@@ -21,6 +21,69 @@ public class AppDispositivos {
             dev.getActualStatus();
             dev.TurnOff();
 
+            System.out.println("------------------");
+
+        }
+        Devices proyector = new Devices("Optoma") {
+            @Override
+            public void TurnOn() {
+                if (isStatus()){
+
+                    System.out.println("El proyector ya esta encendido...");
+
+                }else {
+
+                    System.out.println("Encendiendo proyector con ajuste automático de brillo...");
+                    setStatus(true);
+
+                }
+            }
+        };
+
+        RemoteControl control_proyector = new RemoteControl() {
+            @Override
+            public void Sync() {
+                System.out.println("Sincronizando proyector con control remoto de presentación...");
+            }
+        };
+
+        Devices horno = new Devices("BALAY hot") {
+            @Override
+            public void TurnOn() {
+                if (isStatus()){
+
+                    System.out.println("El horno ya está encendido.");
+
+                }else {
+
+                    System.out.println("Calentando horno con ajuste automático de temperatura...");
+                    setStatus(true);
+
+                }
+            }
+        };
+        devicesList.add(horno);
+        devicesList.add(proyector);
+
+        for (Devices dev : devicesList){
+
+            dev.TurnOn();
+
+            if (dev instanceof RemoteControl){
+
+                ((RemoteControl) dev).Sync();
+
+            } else if (dev.getName().equalsIgnoreCase("Optoma")) {
+
+                control_proyector.Sync();
+
+            }
+
+            dev.getActualStatus();
+            dev.TurnOff();
+
+            System.out.println("------------------");
+
         }
     }
 }
